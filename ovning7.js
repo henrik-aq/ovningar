5c41d536e41dc9b16971fcac292591bbab898b1b
Maia <medeea@rocketmail.com>
2017-10-11 13:35:46 +0200

    Add files via upload

---
+++ ovning7.js
@@ -0,0 +1,35 @@
+var t1 = [1, 2, 3, 4, 5, 6]; // tarning nr 1
+var t2 = [1, 2, 3, 4, 5, 6]; //tarning nr 2
+
+do {
+
+	for (var value of t1) {   
+
+		var rand1 = t1[Math.floor(Math.random() * t1.length)];
+	
+	}
+
+
+	for (var value of t2) {
+
+		var rand2 = t2[Math.floor(Math.random() * t2.length)];
+
+	}
+
+	console.log(rand1, rand2);
+    
+}
+
+while(rand1 !== rand2) 
+
+	
+
+
+
+
+
+
+
+
+
+
